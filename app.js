a1619fee8525b602e01c9e5cff5403fd2e28aca8
ErickCode08 <117024779+ErickCode08@users.noreply.github.com>
2025-02-28 15:38:54 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ document.getElementById('formulario-contacto').addEventListener('submit', functi
     alert('¡Gracias por contactarnos! Pronto nos comunicaremos contigo.');
     this.reset();
 });
+
+const navToggle = document.querySelector(".nav-toggle")
+const navLinks = document.querySelector(".nav-links")
+
+navToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-links_visible");
+
+    if (navLinks.classList.contains("nav-links_visible")) {
+        navToggle.setAttribute("aria-label", "Cerrar menú");
+    }
+});
+
+
